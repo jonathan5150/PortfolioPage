263c2c40859a13be0e0df0b975f5066682917bf5
jonathan5150 <jonathanmichael5150@gmail.com>
2023-09-27 00:03:17 -0500

    Added a navbar

---
+++ connections-clone/src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <h1>Connections Clone</h1>
+            <div className="login-button">
+                <button>Login</button>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
