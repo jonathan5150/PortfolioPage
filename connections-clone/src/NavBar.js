7e90343c1ee27a2754fc9bcbe7f8c3309ed44d86
jonathan5150 <jonathanmichael5150@gmail.com>
2023-09-27 00:52:05 -0500

    Added a working hamburger menu with links that aren't connected to anything yet.

--- connections-clone/src/NavBar.js
+++ connections-clone/src/NavBar.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav className="navbar">
-            <h1>Connections Clone</h1>
+        <nav className={`navbar${isMenuOpen ? ' open' : ''}`}>
+            <div className="navbar-left">
+                <button className="hamburger-menu" onClick={toggleMenu}>
+                    &#9776;
+                </button>
+                <h1>Connections Clone</h1>
+            </div>
+            <div className={`menu${isMenuOpen ? ' open' : ''}`}>
+                <button className="close-button" onClick={toggleMenu}>
+                    ×
+                </button>
+                <ul>
+                    <li><a href="/">Create New Puzzle</a></li>
+                    <li><a href="/">How to Play</a></li>
+                    <li><a href="/">About This Project</a></li>
+                    <li><a href="/">Hi Hannie</a></li>
+                </ul>
+            </div>
             <div className="login-button">
                 <button>Login</button>
             </div>
