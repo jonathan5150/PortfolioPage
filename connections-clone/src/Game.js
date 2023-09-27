06f825fad9ccb242961b513a70f2539de0045479
jonathan5150 <jonathanmichael5150@gmail.com>
2023-09-27 02:34:20 -0500

    Final updates for the night if I changed anything

--- connections-clone/src/Game.js
+++ connections-clone/src/Game.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Game.css'; // Import your CSS file for styling
 
 function Game() {
-    // Function to generate buttons
+    const [buttonToggles, setButtonToggles] = useState(Array(16).fill(false)); // Initialize toggle state for 16 buttons
+
+    // Function to handle button toggle
+    const handleButtonToggle = (index) => {
+        const updatedToggles = [...buttonToggles];
+        const numToggled = updatedToggles.filter((toggled) => toggled).length; // Count currently toggled buttons
+
+        if (numToggled < 4 || updatedToggles[index]) {
+            // Allow toggling if fewer than 4 are toggled or if toggling off a previously toggled button
+            updatedToggles[index] = !updatedToggles[index];
+            setButtonToggles(updatedToggles);
+        }
+    };
+
+    // Function to generate buttons with toggles
     const generateButtons = () => {
         const buttons = [];
         for (let i = 1; i <= 16; i++) {
             buttons.push(
-                <button key={i} className="button">
-                    Button {i}
+                <button
+                    key={i}
+                    className={`button ${buttonToggles[i - 1] ? 'button-toggled' : ''}`}
+                    onClick={() => handleButtonToggle(i - 1)} // Pass the index to handleButtonToggle
+                >
+                    TEST {i}
                 </button>
             );
         }
