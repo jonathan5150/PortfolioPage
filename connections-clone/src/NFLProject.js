8364137e440abd57ebef820675049813522294a6
jonathan5150 <jonathanmichael5150@gmail.com>
2024-02-15 23:02:17 -0600

    Added PDF resume, still needs formatted

--- connections-clone/src/NFLProject.js
+++ connections-clone/src/NFLProject.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import NavBar from './NavBar';
 
 function NFLProject() {
-
-    return "Surely I can add more code to this tonight";
-
+    return (
+        <div>
+            <nav style={{ position: 'fixed', top: '0', left: '0', width: '100%', backgroundColor: '#333', color: '#fff', padding: '10px 20px', zIndex: '1000' }}>
+                <NavBar />
+            </nav>
+            <div style={{ marginTop: '50px', height: 'calc(100vh - 50px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div style={{ width: '80%', height: '80vh', border: '1px solid #ccc', overflow: 'hidden' }}>
+                    <iframe src="/Rainey, Jonathan - Resume 2024.PDF#view=FitH" title="Resume" style={{ width: '100%', height: '100%', border: 'none' }}></iframe>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-
 export default NFLProject;
