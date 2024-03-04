347d1e5a1f105d2a9a68e29d3e7fe5f738eb5d5d
jonathan5150 <jonathanmichael5150@gmail.com>
2024-03-04 00:57:11 -0600

    Changed resume from pdf to png and formatted properly.

--- main/src/Resume.js
+++ main/src/Resume.js
@@ -3,9 +3,14 @@ import React from 'react';
 function Resume() {
     return (
         <div>
-            <div style={{ marginTop: '20px', height: 'calc(100vh - 50px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {/*<div style={{ marginTop: '20px', height: 'calc(100vh - 50px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <div style={{ width: '80%', height: '80vh', border: '1px solid #ccc', overflow: 'hidden' }}>
                     <iframe src="/Rainey, Jonathan - Resume 2024.PDF#view=FitH#toolbar=0" title="Resume" style={{ width: '100%', height: '100%', border: 'none' }}></iframe>
+                </div>
+            </div>*/}
+            <div className="resume-container">
+                <div className="resume-wrapper">
+                    <img src="Rainey, Jonathan - Resume 2024.png" alt="Resume" className="resume-image"/>
                 </div>
             </div>
         </div>
