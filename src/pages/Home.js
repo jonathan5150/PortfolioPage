43e85a108310ac3e3841fa1913061abd9bf031c9
j.rainey <jonathanmichael5150@gmail.com>
2024-03-07 04:10:18 -0600

    Updated Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="Home">
         <p className="homeParagraph">Welcome to my portfolio! This page exists to showcase my past, current, and future projects.</p>
-        <p className="homeParagraph">I'm a software developer who lives in the St. Louis, MO area. I have experience with modern web technologies such as HTML/CSS/JS, Angular, mySQL, as well as application programming languages such as Java and C++. I'm currently learning React which is what this page was made with. It will always be a work in progress.</p>
+        <p className="homeParagraph">I'm a software developer who lives in the St. Louis, MO area. I have experience with modern JS frameworks such as React and Angular, as well as programming languages such as Java and C++. I'm currently focusing on learning React which is what this page was built with. It will always be a work in progress.</p>
         <p className="homeParagraph">Feel free to contact me with any questions you have, I'm always looking for new opportunities!</p>
     </div>
   );
