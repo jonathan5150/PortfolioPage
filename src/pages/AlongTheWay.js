ee44a840123a90aa30a827b41a2bd4614b18b5fb
jonathan5150 <jonathanmichael5150@gmail.com>
2024-04-29 19:48:53 -0500

    Added text to along the way section

--- src/pages/AlongTheWay.js
+++ src/pages/AlongTheWay.js
@@ -7,7 +7,22 @@ function AlongTheWay() {
   return (
     <div className="ScrollableContainer">
         <div className="AlongTheWay">
+
+            <p>This is "Along The Way", the capstone project I created along with a small group of
+            peers in order to graduate from the LaunchCode Coding Bootcamp based in St. Louis, MO.
+            The project was created because our group enjoyed roadtripping and specifically finding
+            interesting places along our routes. However, we noticed that Google Maps was unable to
+            complete this task. </p>
+
+            <p>Below is an image of a directions request from St. Louis to Seattle. When you search for
+            bakeries along the route, Google only shows you bakeries located in St. Louis. So we
+            needed to figure out a way to show ALL of the bakeries along the route. </p>
+
             <img className="image" src={atw1} alt="Along The Way 1" />
+            <p>
+
+            </p>
+
             <img className="image" src={atw2} alt="Along The Way 1" />
             <img className="image bottom-margin" src={atw3} alt="Along The Way 1" />
         </div>
